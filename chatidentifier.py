3ea44633931af842b81398ed13fce0549fc19097
Stephen Emmons <sa154124@gmail.com>
2022-12-14 17:50:58 -0800

    added comments

--- chatidentifier.py
+++ chatidentifier.py
@@ -9,24 +9,24 @@ from imblearn.under_sampling import RandomUnderSampler
 def classify(xSample, ySample):
     xtrain, xtest, ytrain, ytest = train_test_split(xSample, ySample)
 
-    clf = MultinomialNB().fit(xSample, ySample, sample_weight=1)              #apply naive bayes classifier
-    scores = cross_val_score(clf, xSample, ySample, cv=2, scoring='accuracy') #use 2fold due to dataset being on the small side
+    clf = MultinomialNB().fit(xSample, ySample, sample_weight=1)                #apply naive bayes classifier
+    scores = cross_val_score(clf, xSample, ySample, cv=2, scoring='accuracy')   #use 2fold due to dataset being on the small side
 
     clf.fit(xtrain, ytrain, sample_weight=1)
     ypred = clf.predict(xtest)
 
-    f1avg = 0       #will average scores to get a better picture of accuracy
-    betaavg = 0
+    f1_avg = 0       #will average scores to get a better picture of performance
+    beta_avg = 0
     count = 0
 
     while(count < 10):
-        f1avg += f1_score(ytest, ypred, average='macro')                
-        betaavg += fbeta_score(ytest, ypred, average='macro', beta=2.0) #weights recall over precision
+        f1_avg += f1_score(ytest, ypred, average='macro')                
+        beta_avg += fbeta_score(ytest, ypred, average='macro', beta=2.0)     #weights recall over precision
         count += 1
     
 
-    print("\nF1 Score Average Over 10 Iterations:", f1avg/10)
-    print("F2 Score Average Over 10 Iterations:", betaavg/10)
+    print("\nF1 Score Average Over 10 Iterations:", f1_avg/10)
+    print("F2 Score Average Over 10 Iterations:", beta_avg/10)
     print("2fold Cross Val Scores:", scores)
 
 
@@ -36,7 +36,7 @@ with open('clean.txt', 'r', encoding='utf-8') as f:
 
 # count_vect = CountVectorizer()                               
 tokenizer = TreebankWordTokenizer()                            #Create Tokenizer
-# count_vect.set_params(tokenizer=tokenizer.tokenize)          #Using tfidf_vectorizer instead of tfidf transformer results in marginal accuracy increase
+# count_vect.set_params(tokenizer=tokenizer.tokenize)          #Using tfidf_vectorizer instead of tfidf_transformer results in marginal accuracy increase
 # count_vect.set_params(ngram_range=(1,1))                     
 
 # X_counts = count_vect.fit_transform(text)
@@ -46,6 +46,6 @@ tfidf_vectorizer.set_params(tokenizer=tokenizer.tokenize)
 tfidf_vectorizer.set_params(ngram_range=(1,1))                 #ngram range of 1,1 is optimal. Setting to 1,2 drops accuracy by a at least 10%
 X_tfidf = tfidf_vectorizer.fit_transform(text)
 
-ros = RandomOverSampler()
-xOver, yOver = ros.fit_resample(X_tfidf, lbls)
+ros = RandomOverSampler()                                      #Random Over Sampling gives best accuracy
+xOver, yOver = ros.fit_resample(X_tfidf, lbls)                 #Oversamples minority class as they are generally underrepresented in data
 classify(xOver, yOver)
--- datacleaner.py
+++ datacleaner.py
@@ -10,13 +10,13 @@ with open('chatlogs.csv', 'r', encoding='utf-8') as log:
     log.readline()
 
     for line in log:
-        colList = line.split(',', 4)
-        username = colList[2]
+        colList = line.split(',', 4)                #split into lists with columns as each index
+        username = colList[2]                       #index with username listed
         username = username.replace('"', "")
 
         if username not in dataset:
             usernamelist.append(username)
-            dataset[username] = [colList[4]]
+            dataset[username] = [colList[4]]        #column with msg text
         else:
             dataset[username].append(colList[4])
 
@@ -27,11 +27,11 @@ for username in dataset:  #clean text data, remove links and characters left ove
     dataset[username] = list(map(lambda x: x.replace('\n', ""), dataset[username]))
     dataset[username] = list(map(lambda x: re.sub(r'http\S+', '', x), dataset[username]))
    
-with open('clean.txt', 'w', encoding='utf-8') as f:
+with open('clean.txt', 'w', encoding='utf-8') as f:     #write data to be passed to classifier
     for username in dataset:
-        if len(dataset[username]) > 100:
+        if len(dataset[username]) > 100:                #only write for usernames with more than 100 messages
             for msg in dataset[username]:
-                if len(msg) > 0:
+                if len(msg) > 0:                        #only write if message isn't blank after pre-processing
                     f.write(username)
                     f.write(' ')
                     f.write(msg)
